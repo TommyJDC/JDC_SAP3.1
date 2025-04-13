778dcfd0b1ef8f6fb2c8658aab59242400bf3fc2
InFerno <50930990+dioisconnect@users.noreply.github.com>
2025-04-13 21:47:08 +0200

    standard remixv2

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*", "netlify/**"], // Ignore the netlify functions directory
-  serverBuildTarget: "netlify",
-  server: "./netlify/functions/server.js", // Add server entry point as per new instructions
+  serverBuildTarget: "netlify", // Keep only this for Netlify v2 adapter behavior
 };
