972517ef3bbbff90c4da9edaa5bb7054d12fd190
InFerno <50930990+dioisconnect@users.noreply.github.com>
2025-04-13 20:56:05 +0200

    Add vite-plugin-node-polyfills to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'; // Import the plugin
+
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()], // Corrected duplicate plugins
+  plugins: [
+    remix(),
+    tsconfigPaths(),
+    nodePolyfills({
+      // Options (optional): specify whether to polyfill specific globals
+      globals: {
+        Buffer: true, // Default: true. Example: provide a Buffer polyfill
+        global: true, // Default: true
+        process: true, // Default: true
+      },
+      // Specify whether to polyfill specific protocol imports
+      protocolImports: true, // Default: true. Polyfills imports like "node:fs"
+    }),
+  ],
   server: {
     watch: {
       ignored: ['**/netlify/**'], // Ignore netlify directory for dev server watcher
